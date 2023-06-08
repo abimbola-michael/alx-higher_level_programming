d6f3f1745854712c20b6b0cfa39da91a2a8683c8
Abimbola Michael <abimbolamichael100@gmail.com>
2023-06-08 09:43:15 -0700

    Update

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-if a < b:
-    c = add(a, b)
-    for i in range(4, 6):
-        c = add(c, i)
-    return c
-else:
-    return sub(a, b)
+def magic_calculation(a, b):
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return c
+    else:
+        return sub(a, b)
