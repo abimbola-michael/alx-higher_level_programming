9b61e9207794c24a9040b7696101ed7222cecbde
Abimbola Michael <abimbolamichael100@gmail.com>
2023-06-08 10:24:35 -0700

    Update

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,11 +5,11 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
         optd = {"+": add, "-": sub, "*": mul, "/": div}
-        opt = list(operators_dict.keys())
-        if sys.arg[2] not in opt:
+        opt = list(optd.keys())
+        if sys.argv[2] not in opt:
             print("Unknown operator. Available operators: +, -, * and /")
             sys.exit(1)
         a = int(sys.argv[1])
         o = sys.argv[2]
         b = int(sys.argv[3])
-        print("{} {} {} = {}".format(a, sys.argv[2], b, optd[o](a, b)))
+        print("{} {} {} = {}".format(a, o, b, optd[o](a, b)))
