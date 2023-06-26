4f9dda06f91fa64475a45f607cd57c6f986c5d45
Abimbola Michael <abimbolamichael100@gmail.com>
2023-06-26 08:05:37 -0700

    Update

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    for i in range(x):
+    for i in range(0, x):
         try:
             if isinstance(my_list[i], int):
                 count++
