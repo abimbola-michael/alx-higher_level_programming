6b1c46f37fac4efb953252d010644f67906ae20b
Abimbola Michael <abimbolamichael100@gmail.com>
2023-06-27 09:40:37 -0700

    Update

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -46,10 +46,12 @@ class Square:
         return (self.__size * self.__size)
 
     def my_print(self):
-        """Prints # in sauare format"""
+        """Prints # in square format"""
         if self.__size == 0:
             print("")
+        [print("") for h in range(0, self.__position[1])]
         for i in range(0, self.__size):
+            [print(" ", end="") for k in range(0, self.__position[0])]
             for j in range(0, self.__size):
                 print("#", end="")
             print("")
