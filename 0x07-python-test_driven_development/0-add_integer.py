943cd42c0fdd68c53830e0c5e453429f83521903
Abimbola Michael <abimbolamichael100@gmail.com>
2023-07-06 06:30:45 -0700

    Update

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,6 +4,12 @@
 
 def add_integer(a, b=98):
     """a function that adds 2 integers
+    Args:
+        a: first input
+        b: second input
+
+    Raises:
+        TypeError: If either of a or b is a non-integer and a non-float.
     """
     if (not isinstance(a, int) and not isinstance(a, float)):
         raise TypeError("a must be an integer")
