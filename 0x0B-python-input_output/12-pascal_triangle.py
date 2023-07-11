ef6a3fe969d322b978460cff31cb0bb6543a9214
Abimbola Michael <abimbolamichael100@gmail.com>
2023-07-11 07:14:25 -0700

    Update

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -4,17 +4,17 @@ representing the Pascal’s triangle of n"""
 
 
 def pascal_triangle(n):
-    """ pascal triangle"""
+    """pasal triangle"""
 
     if n <= 0:
         return []
-    trgls = [[1]]
 
+    trgls = [[1]]
     while len(trgls) != n:
         trgl = trgls[-1]
         temp_trgl = [1]
         for i in range(len(trgl) - 1):
             temp_trgl.append(trgl[i] + trgl[i + 1])
-            temp_trgl.append(1)
+        temp_trgl.append(1)
         trgls.append(temp_trgl)
     return trgls
