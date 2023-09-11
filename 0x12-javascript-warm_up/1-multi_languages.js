3a4e1cf86a0a69fe8e452d77d9ea5cbf3063fcb5
Abimbola Michael <abimbolamichael100@gmail.com>
2023-09-11 04:45:29 -0700

    Update

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const firstln = “C is fun”;
+const secondln = “Python is cool”;
+const thirdln = “JavaScript is amazing”;
+console.log(firstln);
+console.log(secondln);
+console.log(thirdln);
