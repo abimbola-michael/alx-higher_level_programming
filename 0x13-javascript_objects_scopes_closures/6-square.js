5e3ead428085fc9671c36aca9312474d5dbcc667
Abimbola Michael <abimbolamichael100@gmail.com>
2023-09-12 05:40:21 -0700

    Update

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -5,6 +5,13 @@ class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
+
+  charPrint (c) {
+    const chr = c || 'X';
+    for (let i = 0; i < this.height; i++) {
+      console.log(chr.repeat(this.width));
+    }
+  }
 }
 
 module.exports = Square;
