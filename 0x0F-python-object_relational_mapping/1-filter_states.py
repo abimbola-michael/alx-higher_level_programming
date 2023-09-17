e4977d67bf2ccbf37d29fc7337550dc324719c64
Abimbola <abimbolamichael100@gmail.com>
2023-09-17 04:45:04 -0700

    Update

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" a script that lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ = "__main__":
+    """a script that lists all states with a name starting with N
+    """
+    con = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3]
+            )
+    crs = con.cursor()
+    crs.execute(
+            """SELECT * FROM states WHERE name 
+            LIKE BINARY 'N%' ORDER BY states.id"""
+            )
+    rows = crs.fetchall()
+    for row in rows:
+        print(row)
+        crs.close()
+        con.close()
