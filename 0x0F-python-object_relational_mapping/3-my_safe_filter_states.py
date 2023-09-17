14143d381b7c09365ebc5611d0c1b93ca0ff835d
Abimbola <abimbolamichael100@gmail.com>
2023-09-17 05:15:18 -0700

    Update

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""a script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument
+without SQL Injection"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    """a script that takes in an argument and displays all
+    values in the states
+    """
+    con = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3]
+            )
+    crs = con.cursor()
+    crs.execute(
+            "SELECT * FROM states WHERE name LIKE %s", {argv[4], }
+            )
+    rows = crs.fetchall()
+    for row in rows:
+        print(row)
+    crs.close()
+    con.close()
