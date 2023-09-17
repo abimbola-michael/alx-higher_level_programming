2c854a5a858401f370bd576d6d1fb0833365e6ad
Abimbola <abimbolamichael100@gmail.com>
2023-09-17 06:43:16 -0700

    Update

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+a script that lists all State objects from the database hbtn_0e_6_usa
+"""
+
+if __name__ == "__main__":
+    """a script that lists all State objects from the
+    database hbtn_0e_6_usa
+    """
+
+    from sqlalchemy import create_engine
+    from sqlalchemy.ext.declarative import declarative_base
+    from sqlalchemy.orm import sessionmaker
+    from sys import argv
+    from model_state import Base, State
+
+    engine = create_engine(
+            "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+                argv[1], argv[2], argv[3]
+                )
+            )
+    Session = sessionmaker(bind=engine)
+
+    Base.metadata.create_all(engine)
+
+    session = Session()
+
+    state = session.query(State).first()
+    if state is not None:
+        print("{}: {}".format(state.id, state.name))
+    else:
+        print("Nothing")
+
+    session.close()
