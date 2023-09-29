f9fdb6c88f294d8e87b09505eddcbd91884a2842
Abimbola Michael <abimbolamichael100@gmail.com>
2023-09-29 04:17:58 -0700

    Update

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""script that fetches https://alx-intranet.hbtn.io/status
+using urllib
+"""
+import urllib.request
+
+
+if __name__ == "__main__":
+    req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(req) as res:
+        data = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(data)))
+        print("\t- content: {}".format(data))
+        print("\t- utf8 content: {}".format(data.decode("utf-8")))
