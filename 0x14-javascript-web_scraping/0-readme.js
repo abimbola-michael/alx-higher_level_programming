e02d15abe9d4810e7379c85b471bafae1c84aac5
Abimbola Michael <abimbolamichael100@gmail.com>
2023-10-24 05:39:49 -0700

    Updatwe

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-//a script that reads and prints the content of a file.
+// a script that reads and prints the content of a file.
 
-const fp = process.argv[2]
-const fs = require("fs");
+const fp = process.argv[2];
+const fs = require('fs');
 
-fs.readFile(fp, "utf8", (error, data) => {
+fs.readFile(fp, 'utf8', (error, data) => {
   if (error) {
     console.log(error);
   } else {
